5ea8a2095bd7660492c8ac09d1f28a5820a24a61
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-24 22:29:00 +0900

    Data base fix

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php 
 
-$db = mysqli_connect("localhost","root","","ecom_store");
+$db = mysqli_connect("localhost","root","","ecom");
 
 /// begin getPro functions ///
 
--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php 
 
-$con = mysqli_connect("localhost", "root","","ecom_store")
+$con = mysqli_connect("localhost", "root","","ecom")
 
 ?>
